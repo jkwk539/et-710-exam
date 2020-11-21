cdc5bc72f189e34ceab496b12ef18e4faa943306
jkwk539 <jkwk539@gmail.com>
2020-11-20 20:29:43 -0500

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -25,7 +25,7 @@ export class ProductListComponent {
 
   addToCart(product) {
     this.cartService.addToCart(product);
-    window.alert('Your product has been added to the cart!');
+    window.alert('Click on Checkout to View the Gallery');
   }
 }
 
